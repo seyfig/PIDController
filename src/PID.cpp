8af2a64680fac98034db5aa6a0620643b9b82f0e
seyfi <sgzbyk@gmail.com>
2017-06-03 04:56:47 +0300

    feat: Implement PID controller
    Implement the basic PID controller, able to drive the track wavy and slowly.

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,11 +12,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	p_error = 0.0;
+	d_error = 0.0;
+	i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	i_error += cte;
+	d_error = cte - p_error;
+	p_error = cte;
 }
 
 double PID::TotalError() {
+	return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
